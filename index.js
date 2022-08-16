ff06b5fb21ed0f25d8894d079a0af1b8bd772904
Topo81 <103972679+Topo81@users.noreply.github.com>
2022-08-16 19:21:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let nombre= 'Tomas'; //eslint-disable-line
+let apellido= " Biasutti";
+var estudiante= (nombre.concat(apellido));
+console.log(estudiante);
+let estudianteMayus= estudiante.toUpperCase();
+console.log(estudianteMayus);
+let estudianteMinus=estudiante.toLowerCase();
+var letras= (estudiante.length);
+var primera=(nombre.charAt(0));
+var ultima=(apellido.charAt(8));
+let elimine=(estudiante.trim().length);
+console.log(elimine);
+var contiene=estudiante.includes(nombre);
+console.log(contiene);
+var pepe="juan";
